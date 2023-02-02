2bd6c4094834c5c9c0185d52ddc8ebdea61dd976
alynxlee <alynxlee2@gmail.com>
2023-02-02 12:06:17 +0900

    index skills, title js 수정

--- index.js
+++ index.js
@@ -6,6 +6,67 @@ const sideBar = document.querySelector('.mainFixedBar');
 
 // mainTitle 텍스트 움직임
 
+const el1 = document.querySelector('.titleText img:nth-child(1)');
+const el2 = document.querySelector('.titleText img:nth-child(2)');
+const el3 = document.querySelector('.titleText img:nth-child(3)');
+const el4 = document.querySelector('.titleText img:nth-child(4)');
+const el5 = document.querySelector('.titleText img:nth-child(5)');
+const el6 = document.querySelector('.titleText img:nth-child(6)');
+const el7 = document.querySelector('.titleText img:nth-child(7)');
+const el8 = document.querySelector('.titleText img:nth-child(8)');
+const el9 = document.querySelector('.titleText img:nth-child(9)');
+const el10 = document.querySelector('.titleText img:nth-child(10)');
+const el11 = document.querySelector('.titleText img:nth-child(11)');
+const el12 = document.querySelector('.titleText img:nth-child(12)');
+const el13 = document.querySelector('.titleText img:nth-child(13)');
+const el14 = document.querySelector('.titleText img:nth-child(14)');
+const el15 = document.querySelector('.titleText img:nth-child(15)');
+const el16 = document.querySelector('.titleText img:nth-child(16)');
+const el17 = document.querySelector('.titleText img:nth-child(17)');
+const el18 = document.querySelector('.titleText img:nth-child(18)');
+
+addEventListener('mousemove', e => {
+  let x = e.clientX;
+  let y = e.clientY;
+  // el1.style.transform = `translate(${x / -40}px, ${y / -100}px) scale(0.6)`;
+  // el2.style.transform = `translate(${x / -40}px, ${y / -100}px) scale(0.6)`;
+  // el3.style.transform = `translate(${x / -40}px, ${y / -100}px) scale(0.6)`;
+  // el4.style.transform = `translate(${x / -40}px, ${y / -100}px) scale(0.6)`;
+  // el5.style.transform = `translate(${x / -40}px, ${y / -100}px) scale(0.6)`;
+  // el6.style.transform = `translate(${x / -400}px, ${y / -20}px) scale(0.6)`;
+  // el7.style.transform = `translate(${x / -400}px, ${y / -20}px) scale(0.6)`;
+  // el8.style.transform = `translate(${x / -400}px, ${y / -20}px) scale(0.6)`;
+  // el9.style.transform = `translate(${x / -400}px, ${y / -20}px) scale(0.6)`;
+  // el10.style.transform = `translate(${x / -280}px, ${y / 30}px) scale(0.6)`;
+  // el11.style.transform = `translate(${x / -280}px, ${y / 30}px) scale(0.6)`;
+  // el12.style.transform = `translate(${x / -280}px, ${y / 30}px) scale(0.6)`;
+  // el13.style.transform = `translate(${x / -280}px, ${y / 30}px) scale(0.6)`;
+  // el14.style.transform = `translate(${x / -280}px, ${y / 30}px) scale(0.6)`;
+  // el15.style.transform = `translate(${x / -280}px, ${y / 30}px) scale(0.6)`;
+  // el16.style.transform = `translate(${x / -280}px, ${y / 30}px) scale(0.6)`;
+  // el17.style.transform = `translate(${x / -280}px, ${y / 30}px) scale(0.6)`;
+  // el18.style.transform = `translate(${x / -280}px, ${y / 30}px) scale(0.6)`;
+
+  el1.style.transform = `translate(${x / 100}px, ${y / -100}px) scale(0.6)`;
+  el2.style.transform = `translate(${x / -80}px, ${y / 150}px) scale(0.6)`;
+  el3.style.transform = `translate(${x / 70}px, ${y / -100}px) scale(0.6)`;
+  el4.style.transform = `translate(${x / -100}px, ${y / -30}px) scale(0.6)`;
+  el5.style.transform = `translate(${x / 500}px, ${y / 70}px) scale(0.6)`;
+  el6.style.transform = `translate(${x / -100}px, ${y / 120}px) scale(0.6)`;
+  el7.style.transform = `translate(${x / 450}px, ${y / -50}px) scale(0.6)`;
+  el8.style.transform = `translate(${x / 100}px, ${y / 500}px) scale(0.6)`;
+  el9.style.transform = `translate(${x / 200}px, ${y / -100}px) scale(0.6)`;
+  el10.style.transform = `translate(${x / 280}px, ${y / 200}px) scale(0.6)`;
+  el11.style.transform = `translate(${x / 120}px, ${y / 60}px) scale(0.6)`;
+  el12.style.transform = `translate(${x / -100}px, ${y / -200}px) scale(0.6)`;
+  el13.style.transform = `translate(${x / 150}px, ${y / 150}px) scale(0.6)`;
+  el14.style.transform = `translate(${x / 100}px, ${y / -100}px) scale(0.6)`;
+  el15.style.transform = `translate(${x / -100}px, ${y / 30}px) scale(0.6)`;
+  el16.style.transform = `translate(${x / 120}px, ${y / 100}px) scale(0.6)`;
+  el17.style.transform = `translate(${x / -150}px, ${y / 200}px) scale(0.6)`;
+  el18.style.transform = `translate(${x / -80}px, ${y / 120}px) scale(0.6)`;
+});
+
 addEventListener('scroll', () => {
   // main background 변경
   let num1 = mainContainer.offsetTop - 600;
